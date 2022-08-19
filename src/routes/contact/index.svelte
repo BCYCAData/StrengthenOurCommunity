08263bcffeba758805ef602d3754ca3ed422796a
Alan Keown <bcycadata@outlook.com>
2022-08-19 15:22:42 +1000

    Update Contact page

--- src/routes/contact/index.svelte
+++ src/routes/contact/index.svelte
@@ -19,17 +19,20 @@
 </svelte:head>
 
 <section
-	class="flex flex-col items-center mx-auto mt-10 sm:mt-1 h-full max-w-3xl text-gray-900 text-center"
+	class="flex flex-col items-center mx-auto mt-10 sm:mt-1 h-full max-w-3xl text-gray-800 text-center"
 >
-	<h1>We are here</h1>
+	<h1 class="text-gray-800">If you want to be more involved</h1>
 
-	<h3>We have a project</h3>
-	<p>
-		Our aim is to <br />
-		Prepare
-		<br />
-		Connect
-		<br />
-		Rebound
+	<h1 class="text-gray-800">Please call</h1>
+	<p class="font-semibold text-gray-800">
+		<span class="text-xl text-orange-600">Helen</span>
+		<span>on</span>
+		<span>0424 515 963</span>
+	</p>
+	<h3 class=" text-gray-800">or</h3>
+	<p class="font-semibold text-gray-800">
+		<span class="text-xl text-orange-600">Christine</span>
+		<span>on</span>
+		<span>0488 288 661</span>
 	</p>
 </section>
