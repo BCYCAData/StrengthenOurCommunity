6c773b0ddfccd56db128de6dbf39f949d3410a8f
Alan Keown <bcycadata@outlook.com>
2023-08-05 13:24:54 +1000

    Contact email

--- src/routes/contact/index.svelte
+++ src/routes/contact/index.svelte
@@ -35,4 +35,13 @@
 		<span>on</span>
 		<span>0488 288 661</span>
 	</p>
+	<h1
+		class=" text-l cursor-pointer max-w-80 no-underline hover:underline mt-5 p-2 font-medium text-stone-100 bg-orange-500 rounded-xl"
+	>
+		<span>
+			<a href="mailto:bcyca17@gmail.com?subject=Contacting%20You%20Regarding%20the%20SOC%20Website">
+				Tap here to send us an email
+			</a>
+		</span>
+	</h1>
 </section>
